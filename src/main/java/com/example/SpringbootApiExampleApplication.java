b80da3103f7fceaacf2de98bf708766e0c95f230
Joseph <nguld@itsj-group.com>
2021-02-26 11:39:42 +0700

    clean up

--- src/main/java/com/example/SpringbootApiExampleApplication.java
+++ src/main/java/com/example/SpringbootApiExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringnootApiExampleApplication {
+public class SpringbootApiExampleApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringnootApiExampleApplication.class, args);
+    SpringApplication.run(SpringbootApiExampleApplication.class, args);
   }
 
 }
