139b3c72647a65d787169fbe47693cc42454dc4d
Joseph <nguld@itsj-group.com>
2021-02-26 11:36:44 +0700

    init commit

---
+++ src/main/java/com/example/SpringnootApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringnootApiExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringnootApiExampleApplication.class, args);
+  }
+
+}
